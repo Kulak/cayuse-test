c0b7b56b60cb0ac75ab2eeac943f1f6e59726e79
unknown <C:\Users\Sergei\AppData\Roaming\The Bat!>
2018-12-06 23:31:24 -0800

    functional web service test

--- Demo-WebApp-Test/UnitTest1.cs
+++ Demo-WebApp-Test/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
+using System.Net.Http;
 using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Demo_WebApp;
+using System.Net;
+using Xunit.Abstractions;
 
 namespace Demo_WebApp_Test
 {
-    public class UnitTest1
+    public class UnitTest1 : IClassFixture<WebApplicationFactory<Startup>>
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly HttpClient _client;
+        private readonly ITestOutputHelper _output;
 
+        public UnitTest1(WebApplicationFactory<Startup> factory, ITestOutputHelper output) {
+            _client = factory.CreateClient();
+            _output = output;
+        }
+
+        [Fact]
+        public async void Test1()
+        {
+            var response = _client.GetAsync("/api/values").Result;
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var body = await response.Content.ReadAsStringAsync();
+            _output.WriteLine($"body: {body}");
+            Assert.Equal(body, "[\"value1\",\"value2\"]");
         }
     }
 }
